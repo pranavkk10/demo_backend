5ec787225a2e5acd3cf75730637a58fb76a3e6b0
Pranav Kanumuri <pranavkk@email.sc.edu>
2026-03-26 08:59:40 -0400

    Mongoose

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-var http = require("http");
-const port = process.env.PORT || 3001;
+  const mongoose = require("mongoose");
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
-  })
-  .listen(port);
+//testdb is name of database, it will automatically make it
+mongoose
+  .connect("mongodb+srv://pkanumuri02_db_user:Kanumuri1@sportscarddatabse.kiemnnj.mongodb.net/")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
+
+const schema = new mongoose.Schema({
+  name: String,
+});
+
+async function createMessage() {
+  const result = await message.save();
+  console.log(result);
+}
+
+//this creates a Message class in our app
+const Message = mongoose.model("Message", schema);
+const message = new Message({
+  name: "Hello World",
+});
+
+createMessage();
