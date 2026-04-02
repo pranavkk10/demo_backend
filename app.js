45cdd7a0d34d02bfa658bdc054d4551f8c586c55
Pranav Kanumuri <pranavkk@email.sc.edu>
2026-04-02 08:40:06 -0400

    changes

--- app.js
+++ app.js
@@ -1,24 +1,106 @@
-  const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
 
-//testdb is name of database, it will automatically make it
-mongoose
-  .connect("mongodb+srv://pkanumuri02_db_user:Kanumuri1@sportscarddatabse.kiemnnj.mongodb.net/")
-  .then(() => console.log("Connected to mongodb..."))
-  .catch((err) => console.error("could not connect ot mongodb...", err));
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./public/images/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+const upload = multer({ storage: storage });
 
-const schema = new mongoose.Schema({
-  name: String,
+let houses = [
+    {
+        "_id":1,
+        "name": "Farmhouse",
+        "size": 2000,
+        "bedrooms": 3,
+        "bathrooms": 2.5,
+        "features": [
+            "wrap around porch",
+            "attached garage"
+        ],
+        "main_image": "farm.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "farm-floor1.webp"
+            },
+            {
+                "name": "Basement",
+                "image": "farm-floor2.webp"
+            }
+        ]
+    },
+    {
+        "_id":2,
+        "name": "Mountain House",
+        "size": 1700,
+        "bedrooms": 3,
+        "bathrooms": 2,
+        "features": [
+            "grand porch",
+            "covered deck"
+        ],
+        "main_image": "mountain-house.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "mountain-house1.webp"
+            },
+            {
+                "name": "Optional Lower Level",
+                "image": "mountain-house2.webp"
+            },
+            {
+                "name": "Main Level Slab Option",
+                "image": "mountain-house3.jpg"
+            }
+        ]
+    },
+    {
+        "_id":3,
+        "name": "Lake House",
+        "size": 3000,
+        "bedrooms": 4,
+        "bathrooms": 3,
+        "features": [
+            "covered deck",
+            "outdoor kitchen",
+            "pool house"
+        ],
+        "main_image": "farm.webp",
+        "floor_plans": [
+            {
+                "name": "Main Level",
+                "image": "lake-house1.webp"
+            },
+            {
+                "name": "Lower Level",
+                "image": "lake-house2.webp"
+            }
+        ]
+    }
+]
+
+app.get("/api/houses",(req,res)=>{
+  res.send(houses);
 });
 
-async function createMessage() {
-  const result = await message.save();
-  console.log(result);
-}
-
-//this creates a Message class in our app
-const Message = mongoose.model("Message", schema);
-const message = new Message({
-  name: "Hello World",
+app.get("/api/houses/:id", (req,res)=>{
+  const house=houses.find((h)=>h._id===parseInt(req.params.id));
+  res.send(house);
 });
 
-createMessage();
+//listen for incoming requests
+app.listen(3001, ()=>{
+  console.log("Server is up and running");
+});
